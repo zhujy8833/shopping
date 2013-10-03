564014725694e0a9274de125784ae31ada926e06
jiangyue <zhujy8833@gmail.com>
2013-10-02 23:15:40 -0700

    some ui tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -19,10 +19,29 @@ require.config({
 
 });
 
+require(["jquery"], function($){
+
+});
+
 require(["router/items_router", "router/new_item_router", "router/main_router"],
     function(ItemsRouter, NewItemRouter, MainRouter){
     var mainRouter = new MainRouter();
     var itemRouter = new ItemsRouter();
     var newItemRouter = new NewItemRouter();
     Backbone.history.start();
+    $(document).ready(function(){
+        $(".header").on("click", "li.menu-li", function(){
+            var route;
+            var segment = "";
+            route = $(this).find("a").attr("class").split("-")[1];
+            if(route === 'home'){
+                segment = ""
+            } else if(route === 'view'){
+                segment = "items"
+            } else if(route === 'new'){
+                segment = "new";
+            }
+            Backbone.history.navigate(segment, {trigger: true});
+        });
+    });
 })
